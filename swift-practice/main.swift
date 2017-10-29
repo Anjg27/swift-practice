da5dc3343923a021e5630bc4fdde398bc36524e0
anjg27 <anjalig27@gmail.com>
2017-10-29 13:49:43 -0700

    Initial commit

---
+++ swift-practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-practice
+//
+//  Created by UCode on 10/29/17.
+//  Copyright © 2017 Anjali. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
